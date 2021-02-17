83981309dd637c1f5e214a2f06856da24a4af70e
AlexFrundin <aleksey.frundin@gmail.com>
2021-02-17 15:53:00 +0200

    Local Sever Update Settings

--- migrations/env.py
+++ migrations/env.py
@@ -1,14 +1,18 @@
+import os
+import sys
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from alembic import context
-import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 
-from app.models import Base
 from app.settings import DB_HOST, DB_NAME, DB_PASS, DB_USER, DB_PORT
+from app.models import Base
 
+from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
