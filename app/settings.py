45ef75f08cd28485912b0e348a16fe9e513e3316
frundin_aleksey <aleksey.frundin@gmail.com>
2021-02-17 11:51:58 +0200

    add algorithm

--- app/settings.py
+++ app/settings.py
@@ -14,7 +14,7 @@ DB_HOST = os.environ.get("HOST") or 'localhost'
 DB_PORT = os.environ.get("PORT") or 5432
 SECRET_KEY = os.environ.get(
     "SECRET_KEY") or "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = os.environ.get("ALGORITHM")
+ALGORITHM = os.environ.get("ALGORITHM") or "HS256"
 
 DEBUG = os.environ.get("DEBUG") or True
 
