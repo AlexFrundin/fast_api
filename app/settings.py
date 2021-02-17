ed8d6bd38d9fd31d7213ac7787002838fc7f0281
frundin_aleksey <aleksey.frundin@gmail.com>
2021-02-17 12:26:08 +0200

    fix alembic

--- app/settings.py
+++ app/settings.py
@@ -11,7 +11,7 @@ DB_USER = os.environ.get("USER_DATABASE") or 'test'
 DB_PASS = os.environ.get("PASSWORD_DATABASE") or 'test123test'
 DB_NAME = os.environ.get("NAME_DATABASE") or 'test'
 DB_HOST = os.environ.get("HOST") or 'localhost'
-DB_PORT = os.environ.get("PORT") or 5432
+DB_PORT = os.environ.get("PORT") or "5432"
 SECRET_KEY = os.environ.get(
     "SECRET_KEY") or "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = os.environ.get("ALGORITHM") or "HS256"
